b722b0e12bc4eca80dbb9de87e4b19ae90d05c78
Geza Kovacs <gkovacs@stanford.edu>
2016-02-13 19:23:49 -0800

    inject into facebook

--- facebook.js
+++ facebook.js
@@ -1,3 +1,44 @@
 (function(){
+  var time_started, root, baseurl, insertBeforeItem, insertIfMissing;
   console.log('facebook content script running');
+  time_started = Date.now();
+  root = typeof exports != 'undefined' && exports !== null ? exports : this;
+  baseurl = 'https://localhost:8081';
+  insertBeforeItem = function(jfeeditem){
+    /*
+    jfeeditem.before $('<iframe>').css({
+      width: '495px'
+      height: '300px'
+    }).attr('src', baseurl + '/facebook_message?time_started=' + time_started).attr('frameBorder', '0').addClass('feedlearnquiz').css({'box-shadow': '0 10px 5px -5px grey'})
+    */
+    var block, iframe;
+    block = $('<div>').css({
+      'padding-bottom': '10px',
+      'background-color': '#e9eaed'
+    });
+    iframe = $('<iframe>').css({
+      width: '495px',
+      height: '400px',
+      'background-color': 'white'
+    }).attr('src', baseurl + '/facebook_message?time_started=' + time_started).attr('frameBorder', '0').addClass('feedlearnquiz');
+    iframe.appendTo(block);
+    jfeeditem.before(block);
+  };
+  root.numitems = 0;
+  insertIfMissing = function(){
+    var i$, ref$, len$, feeditem;
+    for (i$ = 0, len$ = (ref$ = document.querySelectorAll('.mbm._5jmm,.userContentWrapper._5pcr')).length; i$ < len$; ++i$) {
+      feeditem = ref$[i$];
+      if (!feeditem.feedlearninserted) {
+        feeditem.feedlearninserted = true;
+        root.numitems += 1;
+        if (root.numitems % 10 === 5) {
+          insertBeforeItem($(feeditem));
+        }
+      }
+    }
+  };
+  setInterval(function(){
+    return insertIfMissing();
+  }, 1000);
 }).call(this);
--- facebook.ls
+++ facebook.ls
@@ -1,4 +1,48 @@
 console.log 'facebook content script running'
 
-#timestarted = Date.now()
+time_started = Date.now()
 
+root = exports ? this
+
+baseurl = 'https://localhost:8081'
+
+insertBeforeItem = (jfeeditem) ->
+  #jfeeditem.before $('<div>').text('newfoobar')
+  #quizid = randstr()
+  /*
+  jfeeditem.before $('<iframe>').css({
+    width: '495px'
+    height: '300px'
+  }).attr('src', baseurl + '/facebook_message?time_started=' + time_started).attr('frameBorder', '0').addClass('feedlearnquiz').css({'box-shadow': '0 10px 5px -5px grey'})
+  */
+  block = $('<div>').css({
+    'padding-bottom': '10px'
+    'background-color': '#e9eaed'
+  })
+  iframe = $('<iframe>').css({
+    width: '495px'
+    height: '400px'
+    'background-color': 'white'
+  }).attr('src', baseurl + '/facebook_message?time_started=' + time_started).attr('frameBorder', '0').addClass('feedlearnquiz')#.css({'box-shadow': '0 10px 5px -5px grey'})
+  iframe.appendTo(block)
+  jfeeditem.before(block)
+  #.attr('id', quizid)
+  return
+
+root.numitems = 0
+
+insertIfMissing = ->
+  for feeditem in document.querySelectorAll('.mbm._5jmm,.userContentWrapper._5pcr')
+    #jfeeditem = $(feeditem)
+    #if not jfeeditem.attr('feedlearninserted')
+    if not feeditem.feedlearninserted
+      #jfeeditem.attr('feedlearninserted', true)
+      feeditem.feedlearninserted = true
+      root.numitems += 1
+      if root.numitems % 10 == 5
+        insertBeforeItem $(feeditem)
+  return
+
+setInterval ->
+  insertIfMissing()
+, 1000
